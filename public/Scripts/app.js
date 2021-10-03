bb422a78e49bfb5d47d8c6dbc46dc1738974d4b4
nmodi2 <79933465+nmodi2@users.noreply.github.com>
2021-10-02 20:21:16 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+// IIFE Immediatly Invoked Function Expression
+(function() {
+    function start() {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", start);
+
+})();
