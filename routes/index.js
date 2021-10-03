1baa81b26a3d6189bcf63e2a2e61b3c441adb7e5
nmodi2 <79933465+nmodi2@users.noreply.github.com>
2021-10-03 18:06:21 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-    res.render('index', { title: 'Contact' });
+    res.render('contact', { title: 'Contact' });
 });
 
 
